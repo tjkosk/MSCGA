5c8aca9833af2ad92a414008d41cb6d5a7c5fd70
tjkosk <tomi.koskinen@aalto.fi>
2018-07-28 23:48:02 +0300

    Added markov process file

---
+++ markov.py
@@ -0,0 +1,33 @@
+import random as rm
+import numpy as np
+import nltk
+
+
+class Markov(object):
+	def __init__(self,dictFile=""):
+		self.markovDict = {}
+		self.sentLength = 20
+		if dictFile:
+			self.loadDictionary(dictFile)
+
+	def loadDictionary(self,dictFile):
+		filey = open(dictFile,'r')
+		self.markovDict = eval(filey.read())
+
+
+	def generateText(self):
+		key = list(rm.choice(self.markovDict['#BEGIN#']))
+		genStr = " ".join( key )
+		for _ in range( self.sentLength ):
+			newKey = self.markovDict.setdefault( tuple(key), "") 
+			if(newKey == ""):
+				break
+			newVal = rm.choice( newKey )
+			genStr = genStr + " " + newVal
+
+			key.pop(0)
+			key.append(newVal)
+
+		return genStr
+
+
